c66872c872156a633a7e437441870b669c14b2bd
Your Name <your-email@example.com>
2025-03-23 19:31:41 +0530

    Initial commit

---
+++ function.py
@@ -0,0 +1,15 @@
+def buscar(start,end):
+    for i in range(start,end):
+    
+        if(i%3==0):
+         print("bus")
+        elif(i%5==0):
+          print("car")
+
+        else:
+           print(i)
+
+    
+     
+result = buscar(1,15)
+print(result)
