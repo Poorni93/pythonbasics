fad39047f75133c46e48f58cd5baf0783e4fa6ee
Your Name <your-email@example.com>
2025-03-23 14:09:46 +0530

    Initial commit

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+a: int = 5
+print(a,type(a))
+a = "hello"
+print(a,type(a))
+b = 7.8
+b = 7.058900123898976509
+#float will take 15 decimal
+print(b,type(b))
+#type convertion
+#c = input("enter a specific char") 
+"""not declare the typecast it always a str"""
+#print(c,type(c))
+
+#d = float(input("enter the float"))
+"""if u declare the type any other type u assign it throws error"""
+#print(d,type(d))
+
+e: int = 8
+f:str = e
+print(f,type(f))
