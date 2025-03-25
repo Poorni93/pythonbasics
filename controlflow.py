54c94a905ea4144b1fed4f19a0bc98277eaed7c4
Your Name <your-email@example.com>
2025-03-25 18:36:20 +0530

    changes

--- controlflow.py
+++ controlflow.py
@@ -8,20 +8,20 @@ while(i>0):
  
  if(a == "yes"):
      print("order a food")
-     b = input("what do you want vada pongal or idly dosa: ")
+     b = input("what do you want 1.vada 2.pongal or 3.idly 4.dosa: ")
      d = int(input("quantity: "))
-     if(b=="vada"):
+     if(b=="1"):
         c = menu["vada"]
-        money = d*c
-     if(b=="pongal"):
+        money = d*c+money
+     if(b=="2"):
       e = menu["pongal"]
-      money2 = d*e
-     if(b=="idly"):
+      money2 = d*e+money2
+     if(b=="3"):
        f = menu["idly"]
-       money3=d*f
-     if(b=="dosa"):
+       money3=d*f+money3
+     if(b=="4"):
        g = menu["dosa"]
-       money4=d*g     
+       money4=d*g+money4    
  else:
     totalamount = money2+money+money3+money4
     print(totalamount)
