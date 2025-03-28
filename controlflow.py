f0c75aa780d95d09074d685c8704412191551c22
Your Name <your-email@example.com>
2025-03-28 18:34:46 +0530

    today commit

--- controlflow.py
+++ controlflow.py
@@ -2,7 +2,6 @@
 
 menu = {"vada":5,"pongal":50,"dosa":30,"idly":20}
 stack = {"vada":10,"pongal":20,"dosa":10,"idly":15}
-
 money=0
 stackofvada=0
 i = 1
@@ -10,29 +9,40 @@ while(i>0):
  a = input("if you need to order any food type y/Y or n/N: ")
  
  if(a == "y" or a=="Y"):
-     print("order a food menu is 1.vada 2.pongal 3.idly 4.dosa ")
+     print("order a food menu is 1.vada 2.pongal 3.dosa 4.idly")
      ordered = int(input("what do you want: "))
      if(ordered < 5):
          quantity = int(input("quantity: "))
-         if(ordered=="1"):
-             sv=stack["vada"]
-             stackofvada =  sv - quantity
-             print(stackofvada)
-             if(stackofvada>=0):
+         if(ordered==1):
+             if(stack["vada"]>=quantity):
+              stack["vada"] = stack["vada"]-quantity
               c = menu["vada"]
               money = quantity*c+money
              else:
-              print("no stack")
+              print(" vada no stack")
       
-         if(ordered=="2"):
+         if(ordered==2):
+            if(stack["pongal"]>=quantity):
+             stack["pongal"]=stack["pongal"]-quantity
              e = menu["pongal"]
              money = quantity*e+money
-         if(ordered=="3"):
-              f = menu["idly"]
-              money=quantity*f+money
-         if(ordered=="4"):
-          g = menu["dosa"]
-          money=quantity*g+money 
+            else:
+               print("pongal no stack")
+         if(ordered==3):
+              if(stack["dosa"]>=quantity):
+                stack["dosa"]=stack["dosa"]-quantity
+                f = menu["dosa"]
+                money=quantity*f+money
+              else:
+                 print("no stack in dosa")
+         if(ordered==4):
+             if(stack["idly"]>=quantity):
+                stack["idly"]=stack["idly"]-quantity
+                g = menu["idly"]
+                money=quantity*g+money 
+             else:
+                print("no stack in idly")
+
      else:
        print("number not in menu") 
  else:
